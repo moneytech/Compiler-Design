322c889be9bcd6298f7158b71c3bf27ce62936cb
vagdevik <30801221+vagdevik@users.noreply.github.com>
2018-02-16 10:21:32 +0530

    Add files via upload

---
+++ lex_assignment/out_file.c
@@ -0,0 +1,13 @@
+#include    <stdio.h>
+#include   <math.h>
+
+#include  <stdlib.h>
+
+int main()
+{
+
+		int a  =  0,k		 = 12;
+		integer		( 5,12);
+	printf("//123\n");
+}
+void integer(int a,int b){}
---
+++ lex_assignment/p.c
@@ -0,0 +1,19 @@
+#include    <stdio.h>
+#include   <math.h>
+
+/*jkl'
+kl*/
+
+#include  <stdlib.h>
+//vag
+
+int main()
+{
+
+/*dfghnm
+dfghju*/
+		int a  =  0,k		 = 12;
+		integer		( 5,12);
+	printf("//123\n");
+}
+void integer(int a,int b){}
